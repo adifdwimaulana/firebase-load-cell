7f852f9e97626475bdba02959e553cff059fe38e
adifdwimaulana <adifdwimaulana@gmail.com>
2020-12-26 12:54:28 +0700

    hotfix

--- src/views/Home.js
+++ src/views/Home.js
@@ -21,22 +21,23 @@ class Home extends React.Component {
         db.ref('/').on('value', (snap) => {
             console.log(snap.val())
             if(snap.val().Batas != "null"){
-                let percent = (snap.val().Berat / snap.val().Batas) * 100
                 let count = (snap.val().Berat / 1000 * 100)
+                if(count < 0){
+                    count = 0
+                }
                 let status
-                if(count <= percent){
+                if(count <= parseInt(snap.val().Batas)){
                     status = 'ringan'
                 } else {
                     status = 'berat'
                 }
-                this.setState({percentage: percent, status})  
+                this.setState({percentage: count, status})  
             }
             this.setState({
                 weight: snap.val().Berat,
                 location: snap.val().Nomer,
                 type: snap.val().Jenis,
                 name: snap.val().Nama,
-                limit: snap.val().Batas
             })
         })
     }
@@ -47,7 +48,7 @@ class Home extends React.Component {
         return(
             <View style={styles.container}>
                 <Text style={styles.title}>{location != "null" ? location : 'Isi Lokasi Terlebih dahulu!'}</Text>
-                <Text style={styles.percentage}>{percentage != null ? `${percentage} %` : '0%'}</Text>
+                <Text style={styles.percentage}>{percentage != null ? `${percentage.toFixed(2)} %` : '0%'}</Text>
                 <Text style={styles.status}>Status : {status != null ? status : 'Isi Batas Terlebih dahulu!'}</Text>
                 <Text style={styles.small}>Nama Barang : {name != "null" ? name : '-'}</Text>
                 <Text style={styles.small}>Jenis Barang : {type != "null" ? type : '-'}</Text>
